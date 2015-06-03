14d1ad00eb78a1a2f24bb08fbf8b1cccf152f987
Lia Skalkos <hello@liaskalkos.net>
2015-06-03 11:35:17 -0400

    Working

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,11 @@
 # code goes here
+
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0 && number % 5 != 0
+    "Fizz"
+  elsif number % 5 == 0 && number % 3 != 0
+    "Buzz"
+  end
+end
